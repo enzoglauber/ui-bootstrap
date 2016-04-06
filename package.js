6837d6fdd234dcb36255a2eae308c1406d0349b6
Enzo Glauber de Lima Almeida <enzojob@gmail.com>
2016-04-06 15:01:02 -0300

    Version 1.3.1

--- package.js
+++ package.js
@@ -5,17 +5,17 @@ var where = 'client'; // where to install: 'client' or 'server'. For both, pass 
 
 Package.describe({
     name: packageName,
-    summary: 'Version 1.3.0 - ui-bootstrap',
-    version: "1.3.0", //packageJson.version,
+    summary: 'Version 1.3.1 - ui-bootstrap',
+    version: "1.3.1", //packageJson.version,
     git: 'https://github.com/enzoglauber/ui-bootstrap'
 });
 
 Package.onUse(function(api) {
     api.versionsFrom(['METEOR@0.9.0','METEOR@1.0','METEOR@1.2']);
    
-    api.use(['angular'], where);
-    api.use(['angular:angular-animate'], where);
-    api.use(['angular:angular-touch'], where);
+    api.use(['angular@1.3.9_2'], where);
+    api.use(['angular:angular-animate@1.5.3_1'], where);
+    api.use(['angular:angular-touch@1.5.3_1'], where);
    
     api.addFiles(['js/ui-bootstrap-tpls.min.js'], where);
 });
